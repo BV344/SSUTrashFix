3a580257a6fea48ca4cd2952d7594eb4b9fb9dba
BV344 <123995573+BV344@users.noreply.github.com>
2023-10-20 14:26:17 -0700

    Initial Commit

---
+++ TrashFix/TrashFixApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrashFixApp.swift
+//  TrashFix
+//
+//  Created by Brayan Vizcaino on 10/20/23.
+//
+
+import SwiftUI
+
+@main
+struct TrashFixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
